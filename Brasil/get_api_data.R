4bf43952916dd8552c0ebbfe58a76c07dcefc4fc
Gustavo Vital <gustavovital@id.uff.br>
2020-04-06 23:39:25 -0300

    add cities and states

--- Brasil/get_api_data.R
+++ Brasil/get_api_data.R
@@ -6,18 +6,50 @@
 
 # start_time <- Sys.time()
 
+# Pacotes Necessários ----
+
 library(tidyverse)
 library(data.table)
 
+# Base de dados -----
+
 dt = fread("https://data.brasil.io/dataset/covid19/caso-full.csv.gz")
 
+# Base de dados para os estados do Brasil ----
+
+# colnames(dt)
+
 dt %>% 
+  na.omit() %>%
+  filter(is_repeated == FALSE) %>% 
+  mutate(Data = as.Date(date)) %>% 
+  group_by(Data, state) %>% 
+  summarise(Confirmados = sum(last_available_confirmed), 
+            `Novos casos` = sum(new_confirmed),
+            `Novas Mortes` = sum(new_deaths)) -> data_estados
+
+# Base de dados para as cidades do Brasil ----
+
+dt %>% 
+  na.omit() %>%
+  filter(is_repeated == FALSE) %>% 
   mutate(Data = as.Date(date),
-         Confirmados = ifelse(is.na(last_available_confirmed) == TRUE, 0, last_available_confirmed)) %>% 
-  filter(city == 'Niterói' | city == 'Rio de Janeiro' | city == 'São Gonçalo', is_repeated == FALSE) %>% 
-  ggplot(aes(x = Data, y = Confirmados, colour = city)) +
-  geom_line()
+         Confirmados = last_available_confirmed,
+         Cidade = city,
+         `População Estimada` = estimated_population_2019,
+         `Novos casos` = new_confirmed,
+         `Novas Mortes` = new_deaths,
+         `Taxa de Mortalidade` = last_available_death_rate) %>% 
+  select(Data, Confirmados, Cidade, `População Estimada`, `Novos casos`, `Novas Mortes`, `Taxa de Mortalidade`) -> data_cidades
+
+saveRDS(data_estados, "Brasil/data_estados.rds")
+saveRDS(data_cidades, "Brasil/data_cidades.rds")
+
+rm(list = ls())  
 
 # end_time <- Sys.time()
-# end_time - start_time
+# end_time - start_time 
+
+# Time difference of 1.765079 secs
+
 
